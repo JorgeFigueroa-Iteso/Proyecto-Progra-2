f56ae73f832f34e314e7229123db51c57001823f
JorgeFigueroa-Iteso <jorge.figueroa@iteso.mx>
2022-10-31 20:09:24 -0600

    a

--- eval.h
+++ eval.h
@@ -1,9 +1,17 @@
+#include <stddef.h>
+
 #ifndef EVAL_H_
 #define EVAL_H_
 
-typedef enum{true, false} bool;
+typedef enum{false, true} bool;
+
 typedef void * Type;
-typedef struct strArray *Array;
+
+typedef struct strQueue *Queue;
+
+/*		Interfaz		*/
+
+
 
 
 #endif /*EVAL_H_*/
