220e0d46c8ddc6553171c19f6921234cfe9300e8
C1ND3R <89820702+C1ND3R@users.noreply.github.com>
2022-11-01 22:49:17 -0600

    a

--- eval.c
+++ eval.c
@@ -31,6 +31,38 @@ void stackPush(){
 		ptr->val = val;
 		ptr->next= head;
 		head=ptr;
+
+opdr leerInput();
+
+
+/* <--- queue ---> */
+struct nodeQueue{
+	Data data;
+	struct nodeQueue *next;
+}
+
+typedef struct nodeQueue *Node;
+
+struct strQueue{
+	Node last;
+	Node first;
+	int size;
+	size_t size_data;
+}
+
+Queue queueCreate(size_t bytes){
+	Queue new=malloc(sizeof(struct strQueue));
+	new->first=NULL;
+	new->last=NULL;
+	new->size_data=bytes;
+	new->size=0;
+
+	return new;
+}
+
+int queueSize(Queue q){
+	if (q!=NULL){
+		return q->size;
 	}
 	system("timeout /t 3");
 }
