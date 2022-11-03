457c1bb1e40b48c379d743d009d0aaf08a839d35
JorgeFigueroa-Iteso <jorge.figueroa@iteso.mx>
2022-11-03 01:54:57 -0600

    a

--- Backup.c
+++ Backup.c
@@ -1,3 +1,5 @@
+#include "Array.h"
+
 #include<stdio.h>
 #include<ctype.h>
 
@@ -26,6 +28,28 @@ int ordenJerarquico(char x) {
 }
 
 int main() {
+
+    // A
+
+    Array A=createArray(20, 4);
+    int j=0;
+
+    // int n=10;
+    // setArray(A, 21, &n);
+    // n=20;
+    // setArray(A, -1, &n);
+    // n=30;
+    // setArray(A, -2, &n);
+    // int *r;
+    // for (int i=0;i<sizeArray(A); i++){
+    //     r=getArray(A,i);
+    //     printf(" %d ", *r);   
+    // }
+
+    // A
+
+
+
     char input[100];
     char *ptr, x;
     printf("Expresion a evaluar : ");
@@ -34,20 +58,30 @@ int main() {
     ptr = input;
     
     while(*ptr != '\0') {
-        if(isalnum(*ptr))
+        if(isalnum(*ptr)){
+            // Imprime los numeros
             printf("%c ",*ptr);
-        else if(*ptr == '(')
+            setArray(A, j, &ptr);
+            ++j;
+        }
+        else if(*ptr == '('){
             stackPush(*ptr);
+        }
         else if(*ptr == ')') {
             while((x = stackPop()) != '('){
                 // Imprime el contenido de los paréntesis
                 printf("%c ", x);
-            }
+                setArray(A, j, &x);
+                ++j;
+            }   
         }
         else {
             while(ordenJerarquico(stack[top]) >= ordenJerarquico(*ptr)){
                 // Imprime los operadores con su orden respectivo
-                printf("%c ",stackPop());
+                char n = stackPop();
+                printf("%c ", n);
+                setArray(A, j, &n);
+                ++j;
             }
             stackPush(*ptr);
         }
@@ -56,6 +90,17 @@ int main() {
     
     while(top != -1) {
         // Imprime lo que esta fuera de los paréntesis
-        printf("%c ",stackPop());
+        char n = stackPop();
+        printf("%c ",n);
+        setArray(A, j, &n);
+        ++j;
+
+    }
+
+    printf("Array\n");
+    int *r;
+    for (int i=0;i<sizeArray(A); i++){
+        r=getArray(A,i);
+        printf(" %d ", *r);
     }return 0;
 }
